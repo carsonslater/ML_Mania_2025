a2d2c4e9cd7461d9a0cb0cf13a043f4092faf9ee
jonathan-lieb <147206320+jonathan-lieb@users.noreply.github.com>
2024-11-15 10:30:53 -0600

    Women's data in usable form and code to simulate brackets

---
+++ make_women's_data.R
@@ -0,0 +1,177 @@
+# Create Women's Data to Train Model
+# Load tidyverse and progress before using
+
+
+# Create features ---------------------------------------------------------
+# from https://www.kaggle.com/code/flat831/create-feature-womens
+
+result_regular <- read_csv("Data/WRegularSeasonCompactResults.csv")
+seed <- read_csv("Data/WNCAATourneySeeds.csv")
+teams <- read_csv("Data/WTeams.csv")
+
+
+# Create Elo rankings for women -------------------------------------------
+# from https://www.kaggle.com/code/flat831/elo-rating
+result_tourney_womens <- read_csv("Data/WNCAATourneyCompactResults.csv")
+result_regular_womens <- read_csv("Data/WRegularSeasonCompactResults.csv")
+teams_womens <- read_csv("Data/WTeams.csv")
+
+result_regular_womens <- result_regular_womens %>% mutate(type = "regular")
+result_tourney_womens <- result_tourney_womens %>% mutate(type = "tourney")
+
+result_merged_womens <- bind_rows(result_regular_womens, result_tourney_womens) %>% 
+  arrange(Season, DayNum) %>% 
+  mutate(
+    WRating_before = 50,
+    LRating_before = 50,
+    WRating_after = 50,
+    LRating_after = 50
+  )
+  
+update_elo_rating_womens <- function(df, teams, K) {
+  
+  n <- nrow(df)
+  pb <- progress_bar$new(total = n)
+  
+  for (i in 1:n) {
+    
+    WTeamID = df[i,3] %>% as.numeric()
+    LTeamID = df[i,5] %>% as.numeric()
+    
+    WTeamRating_before <- teams %>% filter(TeamID == WTeamID) %>% select(Rating) %>% as.numeric()
+    LTeamRating_before <- teams %>% filter(TeamID == LTeamID) %>% select(Rating) %>% as.numeric()
+    WTeam_num <- teams %>% filter(TeamID == WTeamID) %>% select(num) %>% as.numeric()
+    LTeam_num <- teams %>% filter(TeamID == LTeamID) %>% select(num) %>% as.numeric()
+    
+    WTeamRating_after <- WTeamRating_before + K*(1/(10^((WTeamRating_before-LTeamRating_before)/10)+1))
+    LTeamRating_after <- LTeamRating_before - K*(1/(10^((WTeamRating_before-LTeamRating_before)/10)+1))
+    
+    df[i,10] <- WTeamRating_before
+    df[i,11] <- LTeamRating_before
+    df[i,12] <- WTeamRating_after
+    df[i,13] <- LTeamRating_after
+    
+    
+    teams[WTeam_num,3] <- WTeamRating_after
+    teams[LTeam_num,3] <- LTeamRating_after
+    
+    pb$tick()
+  }
+  
+  return(df)
+}
+
+K <- 1
+
+teams_add_womens <- teams_womens %>% mutate(Rating = 50, num = row_number())
+
+res_womens <- update_elo_rating_womens(result_merged_womens, teams_add_womens, K)
+  
+
+res_regular <- res_womens %>% filter(type == "regular")
+
+tmp <- res_regular %>% select(Season, DayNum, WTeamID, WRating_before, WRating_after) %>% rename(TeamID = WTeamID, Rating_before = WRating_before, Rating_after = WRating_after)
+tmp2 <- res_regular %>% select(Season, DayNum, LTeamID, LRating_before, LRating_after) %>% rename(TeamID = LTeamID, Rating_before = LRating_before, Rating_after = LRating_after)
+
+tmp3 <- bind_rows(tmp, tmp2)
+
+tmp4 <- tmp3 %>% group_by(Season, TeamID) %>% summarise(DayNum = max(DayNum))
+
+elo_womens <- tmp3 %>% 
+  inner_join(tmp4, by = c("Season", "TeamID", "DayNum")) %>% 
+  filter(Season >= 2010) %>% 
+  select(TeamID, Rating_after, Season) %>% 
+  rename(Rating = Rating_after) %>% 
+  left_join(teams_womens, by = c("TeamID")) %>% 
+  select(TeamID, TeamName, Season, Rating)
+  
+elo_womens %>% write_csv("Data/EloRating_womens_10.csv")
+
+
+# Continue Create Features ----------------------------------------------
+elo_rating <- read_csv("Data/EloRating_womens_10.csv")
+
+create_features <- function(team_id, result_regular, seed) {
+  tmp <- result_regular %>% filter(WTeamID == team_id | LTeamID == team_id)
+  seed_tmp <- seed %>% filter(TeamID == team_id) %>% mutate(Seed = as.numeric(substr(Seed, 2, 3))) %>% select(-TeamID)
+  
+  tmp2 <- tmp %>% rename(TeamID = WTeamID, Score = WScore, Opp_TeamID = LTeamID, Opp_Score = LScore) #%>% relocate(TeamID, Score, Opp_TeamID, Opp_Score)
+  tmp3 <- tmp %>% rename(TeamID = LTeamID, Score = LScore, Opp_TeamID = WTeamID, Opp_Score = WScore) #%>% relocate(TeamID, Score, Opp_TeamID, Opp_Score)
+  
+  tmp4 <- bind_rows(tmp2, tmp3) %>% 
+    filter(TeamID == team_id) %>% 
+    mutate(
+      diff_score = Score - Opp_Score,
+      win = ifelse(diff_score>0, 1, 0)
+    ) %>% 
+    arrange(Season, DayNum)
+  
+  tmp5 <- tmp4 %>% 
+    group_by(Season) %>% 
+    summarise(
+      count = n(),
+      win_count = sum(win),
+      win_rate = sum(win)/n(),
+      gap_avg = mean(diff_score)
+    ) %>% 
+    left_join(seed_tmp, by = "Season") %>% 
+    mutate(TeamID = team_id)
+  
+  return(tmp5)
+}
+
+
+res <- tibble()
+
+for (team_id in teams$TeamID) {
+  tmp <- create_features(team_id, result_regular, seed)
+  res <- bind_rows(res, tmp)
+}
+
+df_feat <- res %>% 
+  left_join(elo_rating, by =c("TeamID", "Season")) %>%
+  arrange(Season, Seed)
+
+df_feat %>% write_csv("Data/features_womens.csv")
+
+# Create Train Data -----------------------------------------------------
+# from https://www.kaggle.com/code/flat831/create-train-womens
+features <- read_csv("Data/features_womens.csv")
+result <- read_csv("Data/WNCAATourneyCompactResults.csv")
+
+tmp <- result %>% 
+  rename(
+    TeamID_A = WTeamID, TeamID_B = LTeamID, Score_A = WScore, Score_B = LScore
+  )
+
+tmp2 <- result %>% 
+  rename(
+    TeamID_A = LTeamID, TeamID_B = WTeamID, Score_A = LScore, Score_B = WScore
+  )
+
+features_A <- features %>% 
+  select(Season, TeamID, Seed, Rating, win_rate, gap_avg) %>% 
+  filter(is.na(Seed) == 0) %>% 
+  rename(TeamID_A = TeamID, Seed_A = Seed, Rating_A = Rating, win_rate_A = win_rate, gap_avg_A = gap_avg)
+
+features_B <- features %>% 
+  select(Season, TeamID, Seed, Rating, win_rate, gap_avg) %>% 
+  filter(is.na(Seed) == 0) %>% 
+  rename(TeamID_B = TeamID, Seed_B = Seed, Rating_B = Rating, win_rate_B = win_rate, gap_avg_B = gap_avg)
+
+result_merged <- bind_rows(tmp, tmp2) %>% 
+  filter(Season >= 2010) %>% 
+  left_join(features_A, by = c("Season", "TeamID_A")) %>% 
+  left_join(features_B, by = c("Season", "TeamID_B")) %>% 
+  mutate(
+    diff_seed = Seed_A - Seed_B,
+    diff_rating = Rating_A - Rating_B,
+    diff_win_rate = win_rate_A - win_rate_B,
+    diff_gap_avg = gap_avg_A - gap_avg_B,
+    diff_score = Score_A-Score_B,
+    target = ifelse(Score_A-Score_B>0, 1, 0)
+  )
+
+result_merged %>% write_csv("train_womens.csv")
+
+glimpse(result_merged)
---
+++ simulate_n_brackets.R
@@ -0,0 +1,65 @@
+# Load Tidyverse before running this code
+
+# Repeat the process for each round
+make_round_matchups <- function(team_locations){
+  team_locations |> 
+    filter(row_number() %% 2 == 1) |> 
+    rename(team_A = team_id, loc_A = loc) |>
+    bind_cols(team_locations |> 
+                filter(row_number() %% 2 == 0)) |> 
+    rename(loc_B = loc, team_B = team_id)
+}
+
+make_round_preds <- function(round_matchups, all_preds){
+  round_matchups |> 
+    left_join(all_preds, by = c("team_A", "team_B")) |> 
+    mutate(loc = ifelse(win_probA > 0.5, loc_A, loc_B),
+           team_id = ifelse(win_probA > 0.5, team_A, team_B)) |> 
+    select(loc, team_id)
+}
+
+simulate_bracket <- function(team_locations, all_preds){
+  slots <- c("R1W1", "R1W8", "R1W5", "R1W4", "R1W6", "R1W3", "R1W7", "R1W2",
+             "R1X1", "R1X8", "R1X5", "R1X4", "R1X6", "R1X3", "R1X7", "R1X2",
+             "R1Y1", "R1Y8", "R1Y5", "R1Y4", "R1Y6", "R1Y3", "R1Y7", "R1Y2",
+             "R1Z1", "R1Z8", "R1Z5", "R1Z4", "R1Z6", "R1Z3", "R1Z7", "R1Z2",
+             "R2W1", "R2W4", "R2W3", "R2W2", "R2X1", "R2X4", "R2X3", "R2X2",
+             "R2Y1", "R2Y4", "R2Y3", "R2Y2", "R2Z1", "R2Z4", "R2Z3", "R2Z2",
+             "R3W1", "R3W2","R3X1", "R3X2","R3Y1", "R3Y2","R3Z1", "R3Z2",
+             "R4W1","R4X1", "R4Y1","R4Z1","R5WX", "R5YZ","R6CH")
+  results <- vector("list", 6)
+  for(i in 1:6){
+    round_matchups <- make_round_matchups(team_locations)
+    team_locations <- make_round_preds(round_matchups, all_preds)
+    results[[i]] <- team_locations |> select(Team = loc)
+  }
+  bracket <- tibble(Slot = slots,
+                    Team = bind_rows(results)$Team 
+                    )
+}
+
+simulate_n_brackets <- function(team_locations, all_preds, n, bracket_type = "M"){
+  all_brackets <- map(1:n, ~simulate_bracket(team_locations, all_preds), .progress = T)
+  bind_rows(all_brackets) |> 
+    mutate(RowID = row_number(),
+           Tournament = bracket_type,
+           Bracket = rep(1:n, each = 63)) |> 
+    select(RowID, Tournament, Bracket, Slot, Team)
+  
+}
+
+# Example usage
+# Team IDs
+teams <- tibble(team_id = 1:64)
+team_locations <- tibble(team_id = 1:64, loc = rep(c("W", "X", "Y", "Z"), each = 16), 
+                         num = rep(c(1, 16, 8, 9, 5, 12, 4, 13, 6, 11, 3, 14, 7, 10, 2, 15), 4)) |> 
+  mutate(loc = paste0(loc, num)) |> 
+  select(-num)
+
+all_matchups <- expand.grid(team_A = teams$team_id,team_B = teams$team_id) |> 
+  filter(team_A != team_B)
+
+all_preds <- all_matchups |> 
+  mutate(win_probA = runif(4032))
+
+brackets50 <- simulate_n_brackets(team_locations, all_preds, 50)
